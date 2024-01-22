21f26e3a47926ca585684b6e7384c78593457a95
Nathan <nathan.melbeck@gmail.com>
2024-01-22 16:58:43 +0100

    fix: :wrench: échange les contenus de fichiers de test

--- tests/PersonTest.php
+++ tests/PersonTest.php
@@ -4,10 +4,51 @@ namespace Tests;
 
 use PHPUnit\Framework\TestCase;
 
-class PersonTest extends TestCase
-{
-    public function testExemple(): void
-    {
-        $this->assertTrue(false);
+class ExempleTest extends TestCase {
+    private Person[] $peopleTab;
+    private Wallet[] $walletTab;
+
+    //setup
+    public function setUp(): void {
+
+        $thi = new Person('John Doe', 'EUR');
+
     }
+
+    public function testConstructor(): void {
+
+    }
+
+    public function testGetName(): void {
+
+    }
+
+    public function testSetName(): void {
+
+    }
+
+    public function testGetWallet(): void {
+
+    }
+
+    public function testSetWallet(): void {
+
+    }
+
+    public function testGetBalance(): void {
+
+    }
+
+    public function testTransfertFund() {
+
+    }
+
+    public function dividewallet() {
+
+    }
+
+    public function testBuyProduct() {
+
+    }
+
 }
