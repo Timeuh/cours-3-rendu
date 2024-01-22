65332c672fd579720d9b0fa2b287f28e44f9a45f
Sonii79 <sonij590833@gmail.com>
2024-01-22 16:54:39 +0100

    Test Wallet Fini

---
+++ tests/WalletTest.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace Tests;
+
+use App\Entity\Wallet;
+use PHPUnit\Framework\TestCase;
+
+class WalletTest extends TestCase
+{
+    public function testWalletInitialization(): void
+    {
+        $wallet = new Wallet('USD');
+        $this->assertEquals(0, $wallet->getBalance());
+        $this->assertEquals('USD', $wallet->getCurrency());
+    }
+
+    public function testAddFunds(): void
+    {
+        $wallet = new Wallet('USD');
+        $wallet->addFund(100.0);
+        $this->assertEquals(100.0, $wallet->getBalance());
+    }
+
+    public function testRemoveFunds(): void
+    {
+        $wallet = new Wallet('USD');
+        $wallet->addFund(100.0);
+
+        $wallet->removeFund(50.0);
+        $this->assertSame(50.0, $wallet->getBalance());
+    }
+
+    public function testSetBalance(): void
+    {
+        $wallet = new Wallet('USD');
+        $wallet->setBalance(50.0);
+        $this->assertEquals(50.0, $wallet->getBalance());
+    }
+
+    public function testSetCurrency(): void
+    {
+        $wallet = new Wallet('USD');
+        $wallet->setCurrency('EUR');
+        $this->assertEquals('EUR', $wallet->getCurrency());
+    }
+
+    public function testInvalidBalance(): void
+    {
+        $this->expectException(\Exception::class);
+        $wallet = new Wallet('USD');
+        $wallet->setBalance(-50.0);
+    }
+
+    public function testInvalidCurrency(): void
+    {
+        $this->expectException(\Exception::class);
+        $wallet = new Wallet('GBP');
+    }
+
+    public function testInvalidRemoveFunds(): void
+    {
+        $this->expectException(\Exception::class);
+        $wallet = new Wallet('USD');
+        $wallet->removeFund(50.0);
+    }
+
+    public function testInvalidAddFunds(): void
+    {
+        $this->expectException(\Exception::class);
+        $wallet = new Wallet('USD');
+        $wallet->addFund(-50.0);
+    }
+}
