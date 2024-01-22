e4ca08e64c73b1349038e6031678f5705c892608
Sonii79 <sonij590833@gmail.com>
2024-01-22 17:26:13 +0100

    refactor: ajout de setUp

--- tests/WalletTest.php
+++ tests/WalletTest.php
@@ -7,46 +7,48 @@ use PHPUnit\Framework\TestCase;
 
 class WalletTest extends TestCase
 {
+    private Wallet $wallet;
+
+    public function setUp(): void
+    {
+        $this->wallet = new Wallet('USD');
+    }
+
     // Test du constructeur pour initialiser le portefeuille
     public function testWalletInitialization(): void
     {
-        $wallet = new Wallet('USD');
-        $this->assertEquals(0, $wallet->getBalance());
-        $this->assertEquals('USD', $wallet->getCurrency());
+        $this->assertEquals(0, $this->wallet->getBalance());
+        $this->assertEquals('USD', $this->wallet->getCurrency());
     }
 
     // Test de la fonction AddFunds pour ajouter des fonds au portefeuille
     public function testAddFunds(): void
     {
-        $wallet = new Wallet('USD');
-        $wallet->addFund(100.0);
-        $this->assertEquals(100.0, $wallet->getBalance());
+        $this->wallet->addFund(100.0);
+        $this->assertEquals(100.0, $this->wallet->getBalance());
     }
 
     // Test de la fonction RemoveFunds pour retirer des fonds du portefeuille
     public function testRemoveFunds(): void
     {
-        $wallet = new Wallet('USD');
-        $wallet->addFund(100.0);
+        $this->wallet->addFund(100.0);
 
-        $wallet->removeFund(50.0);
-        $this->assertSame(50.0, $wallet->getBalance());
+        $this->wallet->removeFund(50.0);
+        $this->assertSame(50.0, $this->wallet->getBalance());
     }
 
     // Test de la fonction setBalance pour définir le solde du portefeuille
     public function testSetBalance(): void
     {
-        $wallet = new Wallet('USD');
-        $wallet->setBalance(50.0);
-        $this->assertEquals(50.0, $wallet->getBalance());
+        $this->wallet->setBalance(50.0);
+        $this->assertEquals(50.0, $this->wallet->getBalance());
     }
 
     // Test de la fonction setCurrency pour définir la devise du portefeuille
     public function testSetCurrency(): void
     {
-        $wallet = new Wallet('USD');
-        $wallet->setCurrency('EUR');
-        $this->assertEquals('EUR', $wallet->getCurrency());
+        $this->wallet->setCurrency('EUR');
+        $this->assertEquals('EUR', $this->wallet->getCurrency());
     }
 
     // Test d'exception pour la fonction setBalance en cas de solde invalide
@@ -54,8 +56,7 @@ class WalletTest extends TestCase
     {
         $this->expectException(\Exception::class);
         $this->expectExceptionMessage("Invalid balance");
-        $wallet = new Wallet('USD');
-        $wallet->setBalance(-50.0);
+        $this->wallet->setBalance(-50.0);
     }
 
     // Test d'exception pour la fonction setCurrency en cas de devise invalide
@@ -63,7 +64,7 @@ class WalletTest extends TestCase
     {
         $this->expectException(\Exception::class);
         $this->expectExceptionMessage("Invalid currency");
-        $wallet = new Wallet('GBP');
+        $this->wallet->setCurrency('GBP');
     }
 
     // Test d'exception pour la fonction RemoveFunds en cas de montant invalide
@@ -71,8 +72,7 @@ class WalletTest extends TestCase
     {
         $this->expectException(\Exception::class);
         $this->expectExceptionMessage("Invalid amount");
-        $wallet = new Wallet('USD');
-        $wallet->removeFund(-50.0);
+        $this->wallet->removeFund(-50.0);
     }
 
     // Test d'exception pour la fonction RemoveFunds en cas de fonds insuffisants
@@ -80,8 +80,7 @@ class WalletTest extends TestCase
     {
         $this->expectException(\Exception::class);
         $this->expectExceptionMessage("Insufficient funds");
-        $wallet = new Wallet('USD');
-        $wallet->removeFund(50.0);
+        $this->wallet->removeFund(50.0);
     }
 
     // Test d'exception pour la fonction AddFunds en cas de montant invalide
@@ -89,7 +88,6 @@ class WalletTest extends TestCase
     {
         $this->expectException(\Exception::class);
         $this->expectExceptionMessage("Invalid amount");
-        $wallet = new Wallet('USD');
-        $wallet->addFund(-50.0);
+        $this->wallet->addFund(-50.0);
     }
 }
