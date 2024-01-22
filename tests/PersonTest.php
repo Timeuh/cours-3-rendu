6a6625249c51e6f30f87e69a48d483aae3ea2839
Nathan <nathan.melbeck@gmail.com>
2024-01-22 16:54:04 +0100

    init : init fichier test pour Person.php

---
+++ tests/PersonTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Tests;
+
+use PHPUnit\Framework\TestCase;
+
+class PersonTest extends TestCase
+{
+    public function testExemple(): void
+    {
+        $this->assertTrue(false);
+    }
+}
