145523f8e7873cba49e798c56482938736e59df6
Sonii79 <sonij590833@gmail.com>
2024-01-22 17:18:17 +0100

    feat: commentaire et ajout de expectExceptionMessage

--- tests/WalletTest.php
+++ tests/WalletTest.php
@@ -7,6 +7,7 @@ use PHPUnit\Framework\TestCase;
 
 class WalletTest extends TestCase
 {
+    //Test constructeur
     public function testWalletInitialization(): void
     {
         $wallet = new Wallet('USD');
@@ -14,6 +15,7 @@ class WalletTest extends TestCase
         $this->assertEquals('USD', $wallet->getCurrency());
     }
 
+    //Test fonction AddFunds
     public function testAddFunds(): void
     {
         $wallet = new Wallet('USD');
@@ -21,6 +23,7 @@ class WalletTest extends TestCase
         $this->assertEquals(100.0, $wallet->getBalance());
     }
 
+    //Test fonction RemoveFunds
     public function testRemoveFunds(): void
     {
         $wallet = new Wallet('USD');
@@ -30,6 +33,7 @@ class WalletTest extends TestCase
         $this->assertSame(50.0, $wallet->getBalance());
     }
 
+    //Test fonction setBalance
     public function testSetBalance(): void
     {
         $wallet = new Wallet('USD');
@@ -37,6 +41,7 @@ class WalletTest extends TestCase
         $this->assertEquals(50.0, $wallet->getBalance());
     }
 
+    //Test fonction setCurrency
     public function testSetCurrency(): void
     {
         $wallet = new Wallet('USD');
@@ -44,36 +49,46 @@ class WalletTest extends TestCase
         $this->assertEquals('EUR', $wallet->getCurrency());
     }
 
+    //Test fonction exception setBalance
     public function testInvalidBalance(): void
     {
         $this->expectException(\Exception::class);
+        $this->expectExceptionMessage("Invalid balance");
         $wallet = new Wallet('USD');
         $wallet->setBalance(-50.0);
     }
 
+    //Test fonction exception setCurrency
     public function testInvalidCurrency(): void
     {
         $this->expectException(\Exception::class);
+        $this->expectExceptionMessage("Invalid currency");
         $wallet = new Wallet('GBP');
     }
 
+    //Test fonction exception RemoveFunds
     public function testInsufficientRemoveFunds(): void
     {
         $this->expectException(\Exception::class);
-        $wallet = new Wallet('USD');
-        $wallet->removeFund(50.0);
-    }
-
-    public function testNegativeRemoveFunds(): void
-    {
-        $this->expectException(\Exception::class);
+        $this->expectExceptionMessage("Invalid amount");
         $wallet = new Wallet('USD');
         $wallet->removeFund(-50.0);
     }
 
+    //Test fonction exception RemoveFunds
+    public function testNegativeRemoveFunds(): void
+    {
+        $this->expectException(\Exception::class);
+        $this->expectExceptionMessage("Insufficient funds");
+        $wallet = new Wallet('USD');
+        $wallet->removeFund(50.0);
+    }
+
+    //Test fonction exception AddFunds
     public function testInvalidAddFunds(): void
     {
         $this->expectException(\Exception::class);
+        $this->expectExceptionMessage("Invalid amount");
         $wallet = new Wallet('USD');
         $wallet->addFund(-50.0);
     }
